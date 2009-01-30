c8855145704bf8066e28c84b524b79ed6abdcc4a
Keith R. Bennett <kbennett@bbsinc.biz>
2009-01-30 11:27:50 -0500

    More polishing.

--- src/concurrency-examples.clj
+++ src/concurrency-examples.clj
@@ -1,7 +1,13 @@
 ; Simple Illustration of Clojure Concurrency
+;
+; Spawns multiple threads that each increment and log a counter multiple times
+; with intervening sleeps.
+;
+; Keith Bennett (kbennett _at_ bbsinc _dot_ biz)
 
 
 (ns concurrency-example)
+
 
 ; Times logged will be expressed in milliseconds after this time.
 (def reference-time (System/currentTimeMillis))
@@ -38,41 +44,58 @@
 "Runs the counter increment test and its logging num-iterations times
 on a single thread."
 [num-iterations] 
-  (dotimes [i num-iterations]
-    (do
-      (dosync
-        (let [
-          counter-val (alter counter inc)
-          thread-name (str (Thread/currentThread))
-          tm (- (System/currentTimeMillis) reference-time)
-          e (struct log-entry counter-val tm thread-name)
-        ]
-          (println (create-formatted-log-string counter-val tm)) (flush)
-          (alter log conj e)
-        )
-      )
-      (Thread/sleep 500)
-      (recur (dec i))
+  (dotimes [_ num-iterations]
+
+    ; There's a lot in this dosync; if this were a "real" production 
+    ; application, some of this could be removed from the transaction,
+    ; at the expense of a little more complexity (probably an outer 'let').
+    (dosync
+      (let [
+        counter-val (alter counter inc)
+        thread-name (str (Thread/currentThread))
+        tm (- (System/currentTimeMillis) reference-time)
+        entry (struct log-entry counter-val tm thread-name)
+      ]
+        (println (create-formatted-log-string counter-val tm))
+
+        ; Conjoin the log entry to the list.  It will be added to the head of
+        ; the list, so we will reverse the list later to preserve ascending
+        ; chronological order.
+        (alter log conj entry)
       )
     )
+
+    ; The sleep is added here because without it, the first threads created may
+    ; finish before the later threads are created.
+    (Thread/sleep 50)
   )
-  (println (str (Thread/currentThread)) "done") (flush)
+
+  ; Put println in a transaction so the output from the multiple threads
+  ; do not get interleaved.  The flush is there so that the string and its
+  ; new line are output before the next print begins.  (Without the flush,
+  ; every 5 or so runs resulted in twi entries on one line, and then an
+  ; empty blank line.)
+  (dosync (println (str "Thread done: "(Thread/currentThread))) flush)
 )
 
 
-(defn run-test [num-threads]
+(defn run-test 
+"Spawns the threads that will each run do-loop, and returns the list of
+resulting log entries."
+[num-threads]
   (loop [i 0 threads ()]
-    (if (= i num-threads)
-      (do
-        (println (map #(. % join) threads))
-        (println "All " (count threads) " threads joined.")
-        (println @log)
-      )
+    (if (< i num-threads)
       (let [
         f #(do-loop 5)
-        t (Thread. f)]
+        t (Thread. f)
+      ]
         (. t start)
         (recur (inc i) (conj threads t))
+      )
+      (do
+        (println "Waiting for all spawned threads to finish...")
+        (doall (map #(. % join) threads))
+        (println "All " (count threads) " threads finished.")
       )
     )
   )
@@ -81,8 +104,10 @@ on a single thread."
 
 (defn outer-test []
   (let [num-threads 5
-        log (run-test num-threads)]
-    (map #(println %) log)
+        log (run-test num-threads)
+        log-copy (reverse log)
+  ]
+    (doseq [log-entry log-copy] (println log-entry))
   )
 )
 
